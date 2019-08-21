36bbe52601dc3b16aaf9ae6568c06f013aca1c4c
z928385924 <z928385924@163.com>
2019-08-21 14:13:37 +0800

    简书练习完结

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import Header from './common/header';
 import store from './store';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './common/header';
+import Home from './pages/home';
+import Login from './pages/login'
+import Detail from './pages/detail'
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Header/>
+        <BrowserRouter>
+          <Header/>
+          <Route path='/' exact component={Home} />
+          <Route path='/login' exact component={Login} />
+          <Route path='/detail/:id' exact component={Detail} />
+        </BrowserRouter>
       </Provider>
     )
   }
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,24 +1,33 @@
-import React, { Component } from 'react';
+import React, {  PureComponent } from 'react';
 import { connect } from 'react-redux'
 import { HeaderWrapper, Logo, Nav, NavItem, SearchWrapper, NavSearch, Addition, Button, SearchInfo, SearchInfoTitle, SearchInfoSwitch, SearchInfoList, SearchInfoItem } from './style'
 import {CSSTransition} from "react-transition-group";
-import { actionCreators } from './store'
+import { actionCreators } from './store';
+import { Link } from 'react-router-dom'
 
-class Header extends Component {
+class Header extends  PureComponent {
   render() {
+    const { handleInputFocus, list, login } = this.props;
     return (
       <HeaderWrapper>
-        <Logo href='/' />
+        <Link to="/">
+          <Logo/>
+        </Link>
         <Nav>
           <NavItem className='left active'>首页</NavItem>
           <NavItem className='left'>下载App</NavItem>
-          <NavItem className="right">登陆</NavItem>
+          {
+            login ? <NavItem className="right">退出</NavItem> :
+              <Link to="/login">
+                <NavItem className="right">登陆</NavItem>
+              </Link>
+          }
           <NavItem className="right">
             <i className="iconfont">&#xe636;</i>
           </NavItem>
           <SearchWrapper>
             <CSSTransition in={this.props.focused} timeout={200} classNames="slide">
-              <NavSearch className={this.props.focused? 'focused' : ''}  onFocus={this.props.handleInputFocus} onBlur={this.props.handleInputBlur}/>
+              <NavSearch className={this.props.focused? 'focused' : ''}  onFocus={() => handleInputFocus(list)} onBlur={this.props.handleInputBlur}/>
             </CSSTransition>
             <i className={this.props.focused? 'focused zoom iconfont' : 'zoom iconfont'}>&#xe614;</i>
             {this.getListArea()}
@@ -40,8 +49,15 @@ class Header extends Component {
     const jsList = list.toJS();
     const pageList = [];
 
-    for(let i = (page -1)*10; i < page*10; i++) {
-      pageList.push(jsList[i])
+
+    if(page * 10 < jsList.length) {
+      for(let i = (page -1)*10; i < page*10; i++) {
+        pageList.push(jsList[i])
+      }
+    }else {
+      for(let i = (page -1)*10; i < jsList.length; i++) {
+        pageList.push(jsList[i])
+      }
     }
 
     if(focused || mouseIn) {
@@ -52,7 +68,10 @@ class Header extends Component {
         >
           <SearchInfoTitle>
             热门搜索
-            <SearchInfoSwitch onClick={() => handleChangePage(page, totalPage)}>换一批</SearchInfoSwitch>
+            <SearchInfoSwitch onClick={() => handleChangePage(page, totalPage, this.spinIcon)}>
+              <i ref={(icon) => {this.spinIcon = icon}} className="iconfont spin">&#xe851;</i>
+              换一批
+            </SearchInfoSwitch>
           </SearchInfoTitle>
           <SearchInfoList>
             {
@@ -75,15 +94,16 @@ const mapStateToProps = (state) => {
     list: state.get('header').get('list'),
     page: state.get('header').get('page'),
     mouseIn: state.get('header').get('mouseIn'),
-    totalPage: state.get('header').get('totalPage')
+    totalPage: state.get('header').get('totalPage'),
+    login: state.get('login').get('login'),
   }
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     // 聚焦
-    handleInputFocus() {
-      dispatch(actionCreators.getList());
+    handleInputFocus(list) {
+      (list.size === 0) && dispatch(actionCreators.getList());
       dispatch(actionCreators.searchFocus())
     },
     // 失去焦点
@@ -99,7 +119,14 @@ const mapDispatchToProps = (dispatch) => {
       dispatch(actionCreators.mouseLeave())
     },
 
-    handleChangePage(page ,totalPage) {
+    handleChangePage(page ,totalPage, spin) {
+      let originAngle = spin.style.transform.replace(/[^0-9]/ig, '');
+      if( originAngle ) {
+        originAngle = parseInt(originAngle, 10)
+      }else {
+        originAngle = 0;
+      }
+      spin.style.transform = 'rotate(' + (originAngle + 360) + 'deg)';
       if(page < totalPage) {
         dispatch(actionCreators.changePage(page + 1))
       }else {
